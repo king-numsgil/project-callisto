b8d218492b3505d8ab6a2ffea7b1dce5ad21f209
Daniel Grondin <king@numsgil.co>
2020-02-17 20:18:00 -0500

    Removed ImContext.inl, and moved its content to the main header file

--- game/include/umbriel/ImContext.hpp
+++ game/include/umbriel/ImContext.hpp
@@ -32,6 +32,7 @@
  */
 
 #include <Magnum/GL/AbstractShaderProgram.h>
+#include <Corrade/Utility/Resource.h>
 #include <Magnum/GL/Texture.h>
 #include <Magnum/GL/Buffer.h>
 #include <Magnum/Timeline.h>
@@ -435,7 +436,10 @@ namespace umbriel
 		 * otherwise.
 		 */
 		template<class MouseEvent>
-		bool handleMousePressEvent(MouseEvent& event);
+		inline bool handleMousePressEvent(MouseEvent& event)
+		{
+			return handleMouseEvent(event, true);
+		}
 
 		/**
 		 * @brief Handle mouse release event
@@ -447,7 +451,10 @@ namespace umbriel
 		 * otherwise.
 		 */
 		template<class MouseEvent>
-		bool handleMouseReleaseEvent(MouseEvent& event);
+		inline bool handleMouseReleaseEvent(MouseEvent& event)
+		{
+			return handleMouseEvent(event, false);
+		}
 
 		/**
 		 * @brief Handle mouse scroll event
@@ -459,7 +466,17 @@ namespace umbriel
 		 * otherwise.
 		 */
 		template<class MouseScrollEvent>
-		bool handleMouseScrollEvent(MouseScrollEvent& event);
+		inline bool handleMouseScrollEvent(MouseScrollEvent& event)
+		{
+			/* Ensure we use the context we're linked to */
+			ImGui::SetCurrentContext(_context);
+
+			ImGuiIO& io = ImGui::GetIO();
+			io.MousePos = ImVec2(Magnum::Vector2(event.position()) * _eventScaling);
+			io.MouseWheel += event.offset().y();
+			io.MouseWheelH += event.offset().x();
+			return io.WantCaptureMouse;
+		}
 
 		/**
 		 * @brief Handle mouse move event
@@ -471,7 +488,15 @@ namespace umbriel
 		 * otherwise.
 		 */
 		template<class MouseMoveEvent>
-		bool handleMouseMoveEvent(MouseMoveEvent& event);
+		inline bool handleMouseMoveEvent(MouseMoveEvent& event)
+		{
+			/* Ensure we use the context we're linked to */
+			ImGui::SetCurrentContext(_context);
+
+			ImGuiIO& io = ImGui::GetIO();
+			io.MousePos = ImVec2(Magnum::Vector2(event.position()) * _eventScaling);
+			return io.WantCaptureMouse;
+		}
 
 		/**
 		 * @brief Handle key press event
@@ -483,7 +508,10 @@ namespace umbriel
 		 * otherwise.
 		 */
 		template<class KeyEvent>
-		bool handleKeyPressEvent(KeyEvent& event);
+		inline bool handleKeyPressEvent(KeyEvent& event)
+		{
+			return handleKeyEvent(event, true);
+		}
 
 		/**
 		 * @brief Handle key release event
@@ -495,7 +523,10 @@ namespace umbriel
 		 * otherwise.
 		 */
 		template<class KeyEvent>
-		bool handleKeyReleaseEvent(KeyEvent& event);
+		inline bool handleKeyReleaseEvent(KeyEvent& event)
+		{
+			return handleKeyEvent(event, false);
+		}
 
 		/**
 		 * @brief Handle text input event
@@ -507,7 +538,14 @@ namespace umbriel
 		 * otherwise.
 		 */
 		template<class TextInputEvent>
-		bool handleTextInputEvent(TextInputEvent& event);
+		inline bool handleTextInputEvent(TextInputEvent& event)
+		{
+			/* Ensure we use the context we're linked to */
+			ImGui::SetCurrentContext(_context);
+
+			ImGui::GetIO().AddInputCharactersUTF8(event.text().data());
+			return false;
+		}
 
 	private:
 		ImGuiContext* _context;
@@ -522,10 +560,140 @@ namespace umbriel
 
 	private:
 		template<class KeyEvent>
-		bool handleKeyEvent(KeyEvent& event, bool value);
+		inline bool handleKeyEvent(KeyEvent& event, bool value)
+		{
+			/* Ensure we use the context we're linked to */
+			ImGui::SetCurrentContext(_context);
+
+			ImGuiIO& io = ImGui::GetIO();
+
+			switch (event.key())
+			{
+				/* LCOV_EXCL_START */
+				case KeyEvent::Key::LeftShift:
+				case KeyEvent::Key::RightShift:
+					io.KeyShift = value;
+					break;
+				case KeyEvent::Key::LeftCtrl:
+				case KeyEvent::Key::RightCtrl:
+					io.KeyCtrl = value;
+					break;
+				case KeyEvent::Key::LeftAlt:
+				case KeyEvent::Key::RightAlt:
+					io.KeyAlt = value;
+					break;
+				case KeyEvent::Key::LeftSuper:
+				case KeyEvent::Key::RightSuper:
+					io.KeySuper = value;
+					break;
+				case KeyEvent::Key::Tab:
+					io.KeysDown[ImGuiKey_Tab] = value;
+					break;
+				case KeyEvent::Key::Up:
+					io.KeysDown[ImGuiKey_UpArrow] = value;
+					break;
+				case KeyEvent::Key::Down:
+					io.KeysDown[ImGuiKey_DownArrow] = value;
+					break;
+				case KeyEvent::Key::Left:
+					io.KeysDown[ImGuiKey_LeftArrow] = value;
+					break;
+				case KeyEvent::Key::Right:
+					io.KeysDown[ImGuiKey_RightArrow] = value;
+					break;
+				case KeyEvent::Key::Home:
+					io.KeysDown[ImGuiKey_Home] = value;
+					break;
+				case KeyEvent::Key::End:
+					io.KeysDown[ImGuiKey_End] = value;
+					break;
+				case KeyEvent::Key::PageUp:
+					io.KeysDown[ImGuiKey_PageUp] = value;
+					break;
+				case KeyEvent::Key::PageDown:
+					io.KeysDown[ImGuiKey_PageDown] = value;
+					break;
+				case KeyEvent::Key::Enter:
+				case KeyEvent::Key::NumEnter:
+					io.KeysDown[ImGuiKey_Enter] = value;
+					break;
+				case KeyEvent::Key::Esc:
+					io.KeysDown[ImGuiKey_Escape] = value;
+					break;
+				case KeyEvent::Key::Space:
+					io.KeysDown[ImGuiKey_Space] = value;
+					break;
+				case KeyEvent::Key::Backspace:
+					io.KeysDown[ImGuiKey_Backspace] = value;
+					break;
+				case KeyEvent::Key::Delete:
+					io.KeysDown[ImGuiKey_Delete] = value;
+					break;
+				case KeyEvent::Key::A:
+					io.KeysDown[ImGuiKey_A] = value;
+					break;
+				case KeyEvent::Key::C:
+					io.KeysDown[ImGuiKey_C] = value;
+					break;
+				case KeyEvent::Key::V:
+					io.KeysDown[ImGuiKey_V] = value;
+					break;
+				case KeyEvent::Key::X:
+					io.KeysDown[ImGuiKey_X] = value;
+					break;
+				case KeyEvent::Key::Y:
+					io.KeysDown[ImGuiKey_Y] = value;
+					break;
+				case KeyEvent::Key::Z:
+					io.KeysDown[ImGuiKey_Z] = value;
+					break;
+					/* LCOV_EXCL_STOP */
+
+					/* Unknown key, do nothing */
+				default:
+					return false;
+			}
+
+			return io.WantCaptureKeyboard;
+		}
 
 		template<class MouseEvent>
-		bool handleMouseEvent(MouseEvent& event, bool value);
+		inline bool handleMouseEvent(MouseEvent& event, bool value)
+		{
+			/* Ensure we use the context we're linked to */
+			ImGui::SetCurrentContext(_context);
+
+			ImGuiIO& io = ImGui::GetIO();
+			io.MousePos = ImVec2(Magnum::Vector2(event.position()) * _eventScaling);
+
+			std::size_t buttonId;
+			switch (event.button())
+			{
+				case MouseEvent::Button::Left:
+					buttonId = 0;
+					break;
+				case MouseEvent::Button::Right:
+					buttonId = 1;
+					break;
+				case MouseEvent::Button::Middle:
+					buttonId = 2;
+					break;
+
+					/* Unknown button, do nothing */
+				default:
+					return false;
+			}
+
+			/* Instead of setting io.MouseDown directly, we delay this until the
+			   newFrame() call in order to prevent mouse clicks from being ignored when
+			   both a press and a release happens in the same frame. Apart from this
+			   happening when the app can't render fast enough, for some reason it also
+			   happens with SDL2 on macOS -- press delayed by a significant  */
+			_mousePressed.set(buttonId, value);
+			if (value) _mousePressedInThisFrame.set(buttonId, true);
+
+			return io.WantCaptureMouse;
+		}
 	};
 }
 
@@ -595,5 +763,3 @@ namespace Magnum::Math::Implementation
 		}
 	};
 }
-
-#include "ImContext.inl"
